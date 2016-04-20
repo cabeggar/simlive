38742f999fd3b6827fed7ba7a71555ed22ac1862
cabeggar <beggarsky@gmail.com>
2016-04-19 20:20:56 -0400

    generating functions and assigning functions filled

--- openlive/resource_simulator.py
+++ openlive/resource_simulator.py
@@ -2,7 +2,7 @@ import json
 import networkx as nx
 from networkx.readwrite import json_graph
 import ConfigParser
-
+import random
 
 class generator:
     def __init__(self, topo_file=None, topo_type=0):
@@ -25,42 +25,147 @@ class generator:
             f.close()
 
     def _generate_nsf_topology(self):
-        # TODO generate NSF topology
-        # TODO assign weight to 1
-        pass
+        G = nx.Graph()
+        G.add_nodes_from(range(13))
+        G.add_edges_from([(0,1),(0,8),
+                          (1,2),(1,3),
+                          (2,5),
+                          (3,4),(3,10),
+                          (4,5),(4,6),
+                          (5,7),(5,12),
+                          (6,8),
+                          (7,9),
+                          (9,11),(9,13),
+                          (10,13),
+                          (11,12),
+                          (12,13)],
+                          weight=1)
+        _id = 0
+        for u, v in G.edges_iter():
+            G.edge[u][v]['id'] = _id
+            _id += 1
+        for u in G.nodes_iter():
+            G.node[u]['video_src'] = []
+            G.node[u]['user_queries'] = []
+            G.node[u]['clouds'] = 0
+
+        return G
 
     def _generate_fattree_topology(self, port_no=4):
-        # TODO generate fattree topology
-        # TODO assign weight to 1
-        pass
+        edge = port_no*port_no/2
+        aggr = edge
+        core = edge/2
+
+        G = nx.Graph()
+        G.add_nodes_from(range(edge+aggr+core))
+        
+        for i in xrange(port_no):
+            core_switch = edge+aggr
+            for j in xrange(port_no/2):
+                for k in xrange(port_no/2):
+                    # Connect edge switches with aggregation switches
+                    G.add_edge(i*2+j, i*2+edge+k, {'weight': 1})
+                    # Connect aggregation switches with core switches
+                    G.add_edge(i*2+edge+k, core_switch, {'weight': 1})  
+                    core_switch += 1
+       
+        _id = 0
+        for u, v in G.edges_iter():
+            G.edge[u][v]['id'] = _id
+            _id += 1
+        for u in G.nodes_iter():
+            G.node[u]['video_src'] = []
+            G.node[u]['user_queries'] = []
+            G.node[u]['clouds'] = 0
+
+        return G
 
     def _generate_small_world_topology(self, node=20, neigh=4, rewire=0.1):
-        # TODO generate small world topology
-        pass
+        G = nx.watts_strogatz_graph(node, neigh, rewire)
+
+        _id = 0
+        for u, v in G.edges_iter():
+            G.edge[u][v]['id'] = _id
+            _id += 1
+        for u in G.nodes_iter():
+            G.node[u]['video_src'] = []
+            G.node[u]['user_queries'] = []
+            G.node[u]['clouds'] = 0
+
+        return G
 
     def _assign_src(self, src_no):
-        # TODO: randomly pick different sites to attach source
-        pass
+        for i in xrange(src_no):
+            node = random.randint(0, self.topo.number_of_nodes()-1)
+            print node
+            print self.topo.node[node]
+            self.topo.node[node]['video_src'].append(i)
+            print self.topo.node[node]
 
     def _assign_user(self, block_no, user_no, src_no):
-        # TODO: calcualte <block_no> end_points
+        # Find location of blocks
+        if block_no == self.topo.number_of_nodes():
+            blocks = self.topo.nodes_iter()
+        elif block_no > self.topo.number_of_nodes():
+            print "Block number exceeds node numbers"
+        else:
+            blocks = []
+            node0 = random.randint(0, self.topo.number_of_nodes()-1)
+            blocks.append(node0)
+            _paths = nx.single_source_shortest_path(self.topo, node0)
+            paths = []
+            for value in _paths.itervalues():
+                paths.append(value)
+            paths.sort(key = lambda path: len(path))
+            print paths
+            for i in xrange(block_no-1):
+                blocks.append(paths[-i-1][-1])
 
-        # TODO: assign block of users randomly to the endponts
+        # Assign users to blocks
+        users = range(user_no)
+        blocks_users = [[] for _ in xrange(block_no)]
+        for user in users:
+            random.choice(blocks_users).append(user)
 
-        # TODO: attach to self.topo's attribute (userNo)
+        # Assign blocks to nodes
+        for i, block in enumerate(blocks):
+            user_queries = {}
+            for user in blocks_users[i]:
+                # Randomly assign contents to users
+                user_queries[user] = random.randint(0, src_no-1)
+            self.topo.node[block]["user_queries"] = user_queries
 
-        # TODO: randomly assign one source to each user
-        pass
+        # Add userNo attr to graph
+        self.topo.graph["userNo"] = user_no
+
 
     def _assign_cloud_resource(self, cloud_no, avg_comp_resource):
         # TODO: randomly pick cloud_no sites
+        clouds = []
+        if cloud_no == self.topo.number_of_nodes():
+            clouds = self.topo.nodes()
+        elif cloud_no > self.topo.number_of_nodes():
+            print "Error! Cloud numbers exceeds node numbers!"
+        else:
+            node0 = random.randint(0, self.topo.number_of_nodes()-1)
+            clouds.append(node0)
+            _paths = nx.single_source_shortest_path(self.topo, node0)
+            paths = []
+            for value in _paths.itervalues():
+                paths.append(value)
+            paths.sort(key = lambda path: len(path))
+            print paths
+            for i in xrange(cloud_no-1):
+                clouds.append(paths[-i-1][-1])
 
-        # TODO: assign randomly 1/2 avg_comp_resource to 3/2 avg_comp_resource
-        pass
+        for cloud in clouds:
+            self.topo.node[cloud]['clouds'] = random.uniform(0.5*avg_comp_resource, 1.5*avg_comp_resource)
+
 
     def _assign_link_bandwidth(self, avg_link_bandwidth):
         # TODO: assign randomly 1/2 avg_link_bandwidth to 3/2 avg_link_band
-        pass
+        for u, v in self.topo.edges_iter():
+            self.topo.edge[u][v]['bandwidth'] = random.uniform(0.5*avg_link_bandwidth, 1.5*avg_link_bandwidth)
 
     def start_generator(self, config_file, sav_topo):
         config = ConfigParser.ConfigParser()
