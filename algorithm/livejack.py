905faefcc7886043a75d17f8b97e1c4f0b8e67ff
boven <boven.yan@gmail.com>
2017-01-12 09:46:20 -0500

    livejack

--- algorithm/livejack.py
+++ algorithm/livejack.py
@@ -1,28 +1,33 @@
 import cplex
-import networkx as nx
-from topology import Topology
+from math import log
 
 class LiveJack(object):
     def __init__(self, topology, channels, viewers):
         self.topology = topology
 
         self.G = topology.G
-
         self.node_number = self.G.number_of_nodes()
 
         # transfer id based channel to map
-        # [{'sites'->[nodes], 'bw'}]
+        # [{'sites'->[nodes], 'bw'->float, 'src'->node}]
         self.channels = [None] * len(channels)
         self.channel_map = [None] * len(channels)
+
+        self.qoe_cost = None  # TODO: qoe cost
+        self.cost_thres = None
 
         channel_cnt = 0
 
         for channel_id in self.channel:
-            channel_map[channel_cnt] = channel_id
+            self.channel_map[channel_cnt] = channel_id
             channels[channel_cnt] = channels[channel_id]
             channel_cnt = channel_cnt + 1
 
+        # record only the viewers that are directly connected to the server
         self.viewers = viewers
+
+        # [channel_id -> vmf site, ... ]
+        self.local_viewer_map = [{}] * self.node_number
 
     def __serial__(self, channel_id, src, dst):
         return channel_id * self.node_number * self.node_number + \
@@ -46,18 +51,29 @@ class LiveJack(object):
                     link_dst = path[idx+1]
                     self.G[link_src][link_dst]['routes'].add((src, dst))
 
+    def site_delta_constraints(self, viewer_delta):
+        thres_flash_crowds = 10000
+
+        if (viewer_delta >= thres_flash_crowds):
+            return self.node_number
+        else:
+            return int(log(viewer_delta)/log(thres_flash_crowds) *
+                       self.node_number)
+
     def delivery_tree(self):
         prob = cplex.Cplex()
-        prob.objective.set_sense(problem.objective.sense.minimize)
+
+        prob.objective.set_sense(prob.objective.sense.minimize)
 
         rows = []
         cols = []
         vals = []
         my_rhs = []
         row_offset = 0
-        my_ub = [1] * len(self.channels) * node_number * node_number
-        my_lb = [0] * len(self.channels) * node_number * node_number
-        my_ctype = "I" * len(self.channels) * node_number * node_number
+        my_ub = [1] * len(self.channels) * self.node_number * self.node_number
+        my_lb = [0] * len(self.channels) * self.node_number * self.node_number
+        my_ctype = "I" * len(self.channels) * \
+            self.node_number * self.node_number
         my_sense = ""
 
         # tree constraint
@@ -73,7 +89,7 @@ class LiveJack(object):
 
             row_offset = row_offset + 1
 
-        my_sense = my_sense + "E" * len(channels)
+        my_sense = my_sense + "E" * len(self.channels)
 
         # delivery site constraint
         for src in range(self.node_number):
@@ -130,9 +146,9 @@ class LiveJack(object):
         for src in range(len(self.node_number)):
             for dst in range(len(self.node_number)):
                 for channel_idx in range(len(self.node_number)):
-                    my_obj.append(channels[channel_idx]['bw'])
+                    my_obj.append(self.channels[channel_idx]['bw'])
 
-        my_sense = my_sense + "L" * len(G.number_of_edges())
+        my_sense = my_sense + "L" * self.node_number
 
         prob.linear_constraints.add(rhs=my_rhs, senses=my_sense)
 
@@ -144,7 +160,26 @@ class LiveJack(object):
         prob.solve()
         vals = prob.solution.get_values()
 
+    def assign_sites(self):
+        # based on unassigned requests, and existing sites, calculate sites
 
-    def assign_vmf(self):
+        # based on sites, calculate local decision
+        for channel_idx in range(len(self.channels)):
+            sites = self.channels[channel_idx]['sites']
 
-        # out put sites
+            for loc in range(self.node_number):
+                # min affinity
+                min_cost = float("inf")
+                min_site = -1
+
+                for site in sites:
+                    if self.qoe_cost[site][loc] < min_cost:
+                        min_cost = self.qoe_cost[site][loc]
+                        min_site = site
+
+                if min_cost > self.cost_thres:  # for later VMF assignment
+                    min_site = self.channels[channel_idx]['src']
+
+                channel_id = self.channel_map[channel_idx]
+
+                self.local_viewer_map[loc][channel_id] = min_site
