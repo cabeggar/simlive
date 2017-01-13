d35aefb24a9c3463fb22cd314b6e13c334db28dc
boven <boven.yan@gmail.com>
2017-01-13 05:21:34 -0500

    livejack completed

--- algorithm/livejack.py
+++ algorithm/livejack.py
@@ -1,10 +1,13 @@
 import cplex
 from math import log
+import networkx as nx
+
 
 class LiveJack(object):
     def __init__(self, topology, channels, viewers):
         self.topology = topology
 
+        # node attr
         self.G = topology.G
         self.node_number = self.G.number_of_nodes()
 
@@ -29,6 +32,11 @@ class LiveJack(object):
         # [channel_id -> vmf site, ... ]
         self.local_viewer_map = [{}] * self.node_number
 
+        # cost model
+        self.site_cost_scala = 1000
+        self.lasthop_cost_scala = 1
+        # self.trans_cost = None
+
     def __serial__(self, channel_id, src, dst):
         return channel_id * self.node_number * self.node_number + \
             src * self.node_number + dst
@@ -41,10 +49,11 @@ class LiveJack(object):
     def __serial_vmf_assign__(self, channel_id, site_id=None,
                               viewer_id=None):
         if viewer_id is None:
-            return len(self.channels) + self.node_number * self.node_number + \
+            return len(self.channels) * self.node_number * self.node_number + \
                channel_id * self.node_number + site_id
         else:
-            return channel_id * self.node_number + site_id
+            return channel_id * self.node_number * self.node_number + \
+               site_id * self.node_number + viewer_id
 
     def __deserial_vmf_assign__(self, var_id, is_assign=True):
         if is_assign:
@@ -70,6 +79,14 @@ class LiveJack(object):
                     link_src = path[idx]
                     link_dst = path[idx+1]
                     self.G[link_src][link_dst]['routes'].add((src, dst))
+
+    def get_site_cost(self, channel_idx, site_idx):
+        return len(nx.shortest_path(self.G,
+                                    self.channels[channel_idx]['src'],
+                                    self.site_idx))
+
+    def get_trans_cost(self, src, dst):
+        return len(nx.shortest_path(self.G, src, dst))
 
     def site_delta_constraints(self, viewer_delta):
         thres_flash_crowds = 10000
@@ -201,6 +218,7 @@ class LiveJack(object):
         my_ctype = "I" * (len(self.channels) * self.node_number *
                           self.node_number + len(self.channels) *
                           self.node_number)
+        my_sense = ""
 
         # constraints on vmf modifications
         for channel_idx in range(len(self.channels)):
@@ -218,12 +236,94 @@ class LiveJack(object):
                     vals.append(1)
                     rhs_channel = rhs_channel + 1
 
-            my_rhs.append(rhs_channel)
             row_offset = row_offset + 1
+            my_rhs.append(rhs_channel)
+            my_sense = my_sense + "L"
 
         # constraints on aggregate bandwidth
+        for site_idx in range(self.node_number):
 
+            for viewer_idx in range(self.node_number):
+                for channel_idx in range(len(self.channels)):
+                    rows.append(row_offset)
+                    cols.append(self.__serial_vmf_assign__(channel_idx,
+                                                           site_idx,
+                                                           viewer_idx))
+                    vals.append(self.channels[channel_idx]['bw'])
 
+            row_offset = row_offset + 1
+            my_rhs.append(self.G.node[site_idx]['capacity'])
+            my_sense = my_sense + "L"
+
+        # constraints on placement
+        for site_idx in range(self.node_number):
+            for channel_idx in range(self.channels):
+                rows.append(row_offset)
+                cols.append(self.__serial_vmf_assign__(channel_idx,
+                                                       site_idx))
+                vals.append(-1)
+
+                for viewer_idx in range(self.channels):
+                    rows.append(row_offset)
+                    cols.append(self.__serial_vmf_assign__(channel_idx,
+                                                           site_idx,
+                                                           viewer_idx))
+                    vals.append(1)
+
+                row_offset = row_offset + 1
+                my_rhs.append(0)
+                my_sense = my_sense + "L"
+
+        # total viewers
+        for channel_idx in range(len(self.channels)):
+            for viewer_idx in range(self.node_number):
+
+                for site_idx in range(self.node_number):
+                    rows.append(row_offset)
+                    cols.append(self.__serial_vmf_assign__(channel_idx,
+                                                           site_idx,
+                                                           viewer_idx))
+                    vals.append(1)
+
+                row_offset = row_offset + 1
+                my_rhs.append(self.viewers[channel_idx][viewer_idx])
+                my_sense = my_sense + "L"
+
+        # objective
+        my_obj = []
+
+        for channel_idx in range(len(self.channels)):
+
+            for site_idx in range(len(self.node_number)):
+                for viewer_idx in range(len(self.node_number)):
+                    lasthop_cost = self.get_trans_cost(site_idx, viewer_idx)
+
+                    my_obj.append(lasthop_cost *
+                                  self.lasthop_cost_scala)
+
+        for channel_idx in range(len(self.channels)):
+            for site_idx in range(len(self.node_number)):
+                site_cost = self.get_site_cost(channel_idx, site_idx)
+
+                my_obj.append(site_cost * self.site_cost_scala)
+
+        prob.linear_constraints.add(rhs=my_rhs, senses=my_sense)
+
+        prob.variables.add(obj=my_obj, lb=my_lb,
+                           ub=my_ub, types=my_ctype)
+
+        prob.linear_constraints.set_coefficients(zip(rows, cols, vals))
+        prob.solve()
+        vals = prob.solution.get_values()
+
+        # get assignment results
+        for var_idx in range(self.node_number * len(self.channels)):
+            var_id = var_idx + self.node_number *\
+                self.node_number * len(self.channels)
+
+            if vals[var_id] == 1:
+                [channel_id, site_id] = self.__deserial_vmf_assign__(var_id)
+                self.channels[channel_id]['new_site'].insert(site_id)
 
         # based on sites, calculate local decision
         for channel_idx in range(len(self.channels)):
